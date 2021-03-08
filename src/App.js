2ca9e586f633738020985ec3bcf464eb5fec7b43
Hasib787 <mr.hasib45@gmail.com>
2021-03-08 15:39:47 +0600

    making some component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 
 import './App.css';
+import Header from './components/Header/Header';
+import Home from './components/Home/Home';
+import Shipment from './components/Shipment/Shipment';
 
 function App() {
   return (
     <div >
+      <Header></Header>
+      <Home></Home>
+      <Shipment></Shipment>
       
     </div>
   );
